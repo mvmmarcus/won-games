c04031a33e8a667206f84116ae8373b7d415ec5f
Marcus Vinicius Marques <marcus.vinicius.marques@hotmail.com>
2021-07-18 23:12:05 -0300

    feat: add Checkbox component, stories and tests

---
+++ src/components/Checkbox/test.tsx
@@ -0,0 +1,35 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import Checkbox from '.'
+
+describe('<Checkbox />', () => {
+  it('should render with label', () => {
+    renderWithTheme(<Checkbox label="checkbox label" labelFor="check" />)
+
+    // input a partir do papel / role
+    expect(screen.getByRole('checkbox')).toBeInTheDocument()
+
+    // input a partir da label associada
+    expect(screen.getByLabelText(/checkbox label/i)).toBeInTheDocument()
+
+    // label a partir do texto dela
+    expect(screen.getByText(/checkbox label/i)).toHaveAttribute('for', 'check')
+  })
+
+  it('should render without label by default', () => {
+    renderWithTheme(<Checkbox />)
+
+    expect(screen.queryByLabelText('checkbox')).not.toBeInTheDocument()
+  })
+
+  it('should render with black label', () => {
+    renderWithTheme(
+      <Checkbox label="checkbox label" labelFor="check" labelColor="black" />
+    )
+
+    expect(screen.getByText(/checkbox label/i)).toHaveStyle({
+      color: '#030517'
+    })
+  })
+})
