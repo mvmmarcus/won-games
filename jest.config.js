72285aa54e9c17b0e5ea1c65dcc89f3316ddfab1
Marcus Vinicius Marques <marcus.vinicius.marques@hotmail.com>
2021-07-23 02:38:05 -0300

    fix: remve global.ts file from test coverage

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,8 @@ module.exports = {
   collectCoverageFrom: [
     'src/**/*.ts(x)?',
     '!src/**/stories.tsx',
-    '!src/pages/**/*.tsx'
+    '!src/pages/**/*.tsx',
+    '!src/styles/global.ts'
   ],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
   modulePaths: ['<rootDir>/src/', '<rootDir>/.jest/']
